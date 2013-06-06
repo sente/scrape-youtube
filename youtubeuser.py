ae25be6fa4ad3d20d0d785c3014d21708a96cdf5
Stuart Powers <stuart.powers@gmail.com>
2013-06-06 11:01:08 -0500

    alternative/additional scraping logic

--- youtubeuser.py
+++ youtubeuser.py
@@ -47,8 +47,47 @@ class YouTubeUser(object):
         for a in div.xpath('.//ul/li/a'):
             featured_channels.append(a.get('href'))
 
+        for img in div.xpath('.//img[@class="yt-uix-button-icon yt-uix-button-icon-subscribe"]'):
+            print img.get('alt')
+
         self.featured_channels = featured_channels
         return featured_channels
+
+
+
+    def get_featured_channels2(self):
+
+        root = lxml.html.fromstring(self.content)
+
+        div = None
+        for h2 in root.xpath('//h2'):
+            if h2.text_content().strip() == "Featured Channels":
+                div = h2.getparent()
+                break
+
+        if div is None:
+            self.featured_channels = []
+            return []
+
+        featured_channels = []
+
+        features = []
+        for li in div.xpath('.//ul/li'):
+            feature = {}
+            for a in li.xpath('.//a'):
+                print a.get('href')
+                feature['href'] = a.get('href')
+                break
+            for img in li.xpath('.//img'):
+                alt = img.get('alt')
+                if alt.endswith('subscribers'):
+                    print alt
+                    feature['subscribers'] = alt
+
+            features.append(feature)
+
+        self.featured_channels2 = features
+        return features
 
 
 
